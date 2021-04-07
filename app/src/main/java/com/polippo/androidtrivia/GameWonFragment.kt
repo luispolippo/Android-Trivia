d39039efbf7f1bbf382bfe3b010a599841710a9f
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-04-07 01:56:27 -0300

    layouts and fragments implemented

---
+++ app/src/main/java/com/polippo/androidtrivia/GameWonFragment.kt
@@ -0,0 +1,17 @@
+package com.polippo.androidtrivia
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.polippo.androidtrivia.databinding.FragmentGameWonBinding
+
+class GameWonFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        //Inflate layout for this fragment
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game_won, container, false)
+        return binding.root
+    }
+}
